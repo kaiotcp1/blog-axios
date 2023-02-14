7841dfa9d680f0920c485febe9c3b5b46868352b
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-02-14 16:05:16 -0300

    Configurando Navbar

--- blog-axios/src/App.jsx
+++ blog-axios/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
 import { Outlet } from 'react-router-dom'
-
+import Navbar from './components/Navbar'
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <div className="container">
         <Outlet />
       </div>
